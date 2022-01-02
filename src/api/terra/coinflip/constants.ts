d3e8960dd6bae084b71f7e8e174f1972e1d20806
Nikita <30903256+NikitosGolubev@users.noreply.github.com>
2022-01-02 14:17:03 +0300

    Contract address  update for testnet

--- src/api/terra/coinflip/constants.ts
+++ src/api/terra/coinflip/constants.ts
@@ -2,7 +2,7 @@ import { NetworkKey } from '@/constants/networks';
 
 export const MAIN_CONTRACT_ADDRESS: Record<NetworkKey, string> = <const>{
   [NetworkKey.MAINNET]: 'terra1v7767uu6a9pq8u49zes7grra5skvmmmua5x3cj',
-  [NetworkKey.TESTNET]: 'terra1v7767uu6a9pq8u49zes7grra5skvmmmua5x3cj',
+  [NetworkKey.TESTNET]: 'terra1n3gvwgjmhpepjv6jaj7kj3ucnhmadhmu970uej',
 };
 
 export enum ActionType {
